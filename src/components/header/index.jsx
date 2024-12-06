6045632845239c45ba8fb235d26f65acd0075578
Ricardo Potiguar <ricardopotiguar@gmail.com>
2024-12-06 18:56:00 -0300

    Inclusão de menu Lista de necessidade no Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -35,54 +35,60 @@ const Header = () => {
                 <div id='div-h1'>
                     <h1 id='titlePlatform' onClick={() => navigate('/')}>Plataforma de Doações</h1>
                 </div>
-                <nav className="menu">
-                    <button
-                        className="menu-toggle"
-                        onClick={toggleMenu}
-                        aria-label={menuOpen ? "Fechar Menu" : "Abrir Menu"}
-                    >
-                        ☰
-                    </button>
+                <div></div>{console.log()}
+                {isLoggedIn && window.location.pathname == '/' ? (
+                    <div id='div-necessidades' onClick={() => navigate('/donation')}>Lista de Necessidades</div>
+                ) : (                  
+                     <p></p>
+                     )}
+                < nav className="menu">
+                <button
+                    className="menu-toggle"
+                    onClick={toggleMenu}
+                    aria-label={menuOpen ? "Fechar Menu" : "Abrir Menu"}
+                >
+                    ☰
+                </button>
 
-                    {!isLoggedIn ? (
-                        <ul className={`menu-list${menuOpen ? ' active' : ''}`}>
-                            <li><a onClick={() => navigate('/cadastro')}>Criar conta</a></li>
-                            <li><a onClick={() => navigate('/login')}>Entrar</a></li>
-                        </ul>
-                    ) : (
-                        <ul className={`menu-list${menuOpen ? ' active' : ''}`}>
-                            <li><a onClick={() => navigate('/donation')}>Necessidades</a></li>
-                            <li><a onClick={() => {
-                                localStorage.removeItem('donations-token'); // Remove o token no logout
-                                setIsLoggedIn(false); // Atualiza o estado para deslogado
-                                setUserEmail(''); // Limpa o e-mail do usuário
-                                navigate('/login')
-                            }}>Sair</a></li>
-                        </ul>
-                    )}
-                </nav>
                 {!isLoggedIn ? (
-                    // Mostra os botões de login se o usuário não estiver logado
-                    <div className="div-buttons">
-                        <button onClick={() => navigate('/cadastro')}>Criar conta</button>
-                        <button onClick={() => navigate('/login')}>Entrar</button>
-                    </div>
+                    <ul className={`menu-list${menuOpen ? ' active' : ''}`}>
+                        <li><a onClick={() => navigate('/cadastro')}>Criar conta</a></li>
+                        <li><a onClick={() => navigate('/login')}>Entrar</a></li>
+                    </ul>
                 ) : (
-                    // Mostra o e-mail do usuário logado
-                    <div id="container-header-user">
-                        <p>{userEmail}</p>
-                        <button onClick={() => {
+                    <ul className={`menu-list${menuOpen ? ' active' : ''}`}>
+                        <li><a onClick={() => navigate('/donation')}>Necessidades</a></li>
+                        <li><a onClick={() => {
                             localStorage.removeItem('donations-token'); // Remove o token no logout
                             setIsLoggedIn(false); // Atualiza o estado para deslogado
                             setUserEmail(''); // Limpa o e-mail do usuário
                             navigate('/login')
-                        }}>
-                            Sair
-                        </button>
-                    </div>
+                        }}>Sair</a></li>
+                    </ul>
                 )}
-            </div>
-        </header>
+            </nav>
+            {!isLoggedIn ? (
+                // Mostra os botões de login se o usuário não estiver logado
+                <div className="div-buttons">
+                    <button onClick={() => navigate('/cadastro')}>Criar conta</button>
+                    <button onClick={() => navigate('/login')}>Entrar</button>
+                </div>
+            ) : (
+                // Mostra o e-mail do usuário logado
+                <div id="container-header-user">
+                    <p>{userEmail}</p>
+                    <button onClick={() => {
+                        localStorage.removeItem('donations-token'); // Remove o token no logout
+                        setIsLoggedIn(false); // Atualiza o estado para deslogado
+                        setUserEmail(''); // Limpa o e-mail do usuário
+                        navigate('/login')
+                    }}>
+                        Sair
+                    </button>
+                </div>
+            )}
+        </div>
+        </header >
     )
 }
 

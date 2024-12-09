5cd8c97c1f0eafb0b3aa2d19cac0b4cd18661f69
Ricardo Potiguar <ricardopotiguar@gmail.com>
2024-12-09 15:46:15 -0300

    Doantion - Adicionada modal de confirmação e processo de realização de programação de doação.

--- src/pages/donations/index.jsx
+++ src/pages/donations/index.jsx
@@ -4,6 +4,7 @@ import api from '../../services/api.js'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 import SapatoSocial from '../../assets/sapato-social-masculino.avif'
+import { jwtDecode } from 'jwt-decode';
 
 function Donations() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -14,15 +15,19 @@ function Donations() {
     type: ''
   });
   const [totalItems, setTotalItems] = useState(0); // Total de itens no banco
+  const [isModalConfirmationOpen, setIsModalConfirmationOpen] = useState(false);
+  const [userNeedIdForDonation, setUserNeedIdForDonation] = useState('');
+  const [userNeedQuantityForDonation, setUserNeedQuantityForDonation] = useState(''); 
 
   useEffect(() => {
     fetchNeeds();
   }, [searchTerm, currentPage, filters]);
 
 
-  // Função para buscar dados da API
+  // Função para buscar dados da API com relação das necessidades
   const fetchNeeds = async () => {
     try {
+      filters.state = 'pending'
       const queryParams = new URLSearchParams({
         search: searchTerm,
         page: currentPage,
@@ -35,6 +40,30 @@ function Donations() {
       setTotalItems(response.data.totalItems);
     } catch (error) {
       console.error('Erro ao buscar necessidades:', error);
+    }
+  };
+
+  // Função para executar a programação de doação
+  async function createDonation(UserNeedId, UserNeedQuantity) {
+    try {
+      const token = localStorage.getItem('donations-token');
+      let decoded = null
+      if (token) {
+        // Decodifica o token
+        decoded = jwtDecode(token)
+        console.log(decoded)
+      }
+
+      await api.post('/api/v1/doacao', {
+        "donorId": decoded.userId,
+        "userNeedsId": UserNeedId,
+        "quantity": UserNeedQuantity
+      })
+
+    /*   navigate('/sucesso');  */
+
+    } catch (error) {
+      console.error('Erro ao realizar programação de doação:', error);
     }
   };
 
@@ -57,6 +86,24 @@ function Donations() {
       [filterName]: value,
     }));
     setCurrentPage(1); // Reinicia na página 1 após filtrar
+  };
+
+  const handleOpenConfirmationModal = (id, quantity) => {
+    setIsModalConfirmationOpen(true);
+    setUserNeedIdForDonation(id);
+    setUserNeedQuantityForDonation(quantity);
+  };
+
+  const handleCloseConfirmationModal = () => {
+    setIsModalConfirmationOpen(false);
+  };
+
+  const handleConfirmDonation = () => {
+    console.log('Ação confirmada!');
+    console.log('UserNeedIdForDonation', userNeedIdForDonation)
+    console.log('UserNeedQuantityForDonation', userNeedQuantityForDonation)
+    createDonation(userNeedIdForDonation, userNeedQuantityForDonation)
+    setIsModalConfirmationOpen(false);
   };
 
   return (
@@ -95,9 +142,10 @@ function Donations() {
                   <img src={SapatoSocial} alt={need.title} />
                   <h4>{need.title}</h4>
                   <p>{need.description}</p>
+                  <p>Quantidade:  {need.quantity}</p> 
                   <div className='container-card-ultima-linha'>
                     <span className='card-categoria'>{need.type}</span>
-                    <button>Fazer Doação</button>
+                    <button onClick={() => handleOpenConfirmationModal(need.id, need.quantity)}>Fazer Doação</button>
                   </div>
                 </div>
               ))}
@@ -111,7 +159,7 @@ function Donations() {
                 Anterior
               </button>
               <span>Página {currentPage}</span>
-              <button 
+              <button
                 onClick={() => handlePageChange(currentPage + 1)}
                 disabled={currentPage === Math.ceil(totalItems / itemsPerPage)}>
                 Próxima
@@ -119,9 +167,25 @@ function Donations() {
             </div>
           </div>
         </div>
-
-
       </div>
+      {isModalConfirmationOpen && (
+        <div id='modalOverlayStyle'>
+          <div id='modalStyle'>
+            <h2>Confirmar Doação</h2>
+            <p>Você tem certeza que deseja realizar esta ação?</p>
+            <br />
+            <p>Ao confirmar você receberá um e-mail com as informações de contato da pessoa que possui esta necessidade. Esta pessoa também receberá um e-mail com os seus dados, para que possam combinar a melhor forma de realizar a entrega do produto doado ou a prestação do serviço doado.</p>
+            <div id='modalActionsStyle'>
+              <button onClick={handleCloseConfirmationModal} id='cancelButtonStyle'>
+                Cancelar
+              </button>
+              <button onClick={handleConfirmDonation} id='confirmButtonStyle'>
+                Confirmar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <Footer />
     </div>
   )

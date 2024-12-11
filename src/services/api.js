adb25c9ed1b05ef8f3e5c78760483a5985ca3ed8
Ricardo Potiguar <ricardopotiguar@gmail.com>
2024-12-11 15:24:58 -0300

    Ajuste Donations para exibir imagens dinamicas vindas da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://donations-api-mysql.onrender.com'
+/*     baseURL: 'https://donations-api-mysql.onrender.com' */
+    baseURL: 'http://localhost:3000' //local
+    
 })
 
 export default api

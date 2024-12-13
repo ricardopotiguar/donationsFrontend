b39c5902a3322ccbfe7abfdb5f8d6b271746ebe5
Ricardo Potiguar <ricardopotiguar@gmail.com>
2024-12-13 11:16:32 -0300

    ajuste base url API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-/*     baseURL: 'https://donations-api-mysql.onrender.com' */
-    baseURL: 'http://localhost:3000' //local
+     baseURL: 'https://donations-api-mysql.onrender.com';
     
 })
 

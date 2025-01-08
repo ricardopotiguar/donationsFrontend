b57c369e09702e507aed9dba42530cac75115d0d
Ricardo Potiguar <ricardopotiguar@gmail.com>
2025-01-08 15:50:57 -0300

    Ajuste .env, adicionada variavel de ambiente VITE_BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
-/*      baseURL: 'https://donations-api-mysql.onrender.com' */
-     baseURL: 'http://localhost:3000'
-    
+/*      baseURL: 'https://donations-api-mysql.onrender.com'
+     baseURL: 'http://localhost:3000' */
+     baseURL: baseURL,
 })
 
 export default api
